a8350893b27962a4f235ebcff6b098a2c9774838
arhee <arheeee@gmail.com>
2018-04-24 13:38:22 -0700

    create map

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import Create from "./components/Create";
 
-class App extends Component {
+
+
+
+class App extends React.Component {
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src="" className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Create />
       </div>
     );
   }
