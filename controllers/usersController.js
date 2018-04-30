8645b0716c85f4d219e1298b85a3fa3a8d08dee5
arhee <arheeee@gmail.com>
2018-04-30 16:09:16 -0700

    check for error

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,7 +14,11 @@ module.exports = {
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => 
+        {
+          console.log(err)
+          res.status(422).json(err)
+        });
   },
   findUser: function (req, res) {
     console.log(req.params.username)
